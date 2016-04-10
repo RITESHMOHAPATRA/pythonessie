39b31e19ce5d1d6dd0af2c4622184c7dc969d751
alice <alice@wonderland>
2016-04-11 01:47:42 +0300

    delete duplicate and git ignore tmp

--- whosthat.py
+++
@@ -1,9 +0,0 @@
-import urllib
-
-def urlOpener():
-    target = urllib.urlopen('https://raw.githubusercontent.com/nialaa/pythonessie/master/.gitignore', proxies = None)
-    print target.read()
-
-print "Starting..."
-urlOpener()
-
